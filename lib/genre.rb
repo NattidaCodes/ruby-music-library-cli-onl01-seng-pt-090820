c62cacc89b6513699d3dc1038441d76980401263
Nattida Wong <nattidac.wong@gmail.com>
2020-10-13 19:25:19 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,12 +1,29 @@
-# class Genre
-#
-#   attr_accessor :name
-#   attr_reader
-#
-#   @@all = []
-#
-#   def self.all
-#     @@all
-#     end
-#
-# end
+class Genre
+
+  attr_accessor :name
+  attr_reader
+
+  @@all = []
+
+  def self.all
+    @@all
+    end
+
+  def initialize(name)
+    @name = name
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(song_name)
+    new_song = self.new(song_name)
+    new_song.save
+    new_song
+  end
+end
