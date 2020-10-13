e966c7058bc9044e9223c0f574e9004bb699a247
Nattida Wong <nattidac.wong@gmail.com>
2020-10-13 20:28:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,12 +43,12 @@ class Song
     # end
   end
 
-  # def self.find_by_name(name)
-  #   @@all.find{|song| song.name == name}
-  # end
-  #
-  # def self.find_or_create_by_name(name)
-  #   self.find_by_name(name) || self.create(name)
-  # end
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
 
 end
