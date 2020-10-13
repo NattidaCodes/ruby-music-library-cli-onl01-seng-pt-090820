8f8a4fa9db1a5e0ea4164ea7ca9ef62ec83a2085
Nattida Wong <nattidac.wong@gmail.com>
2020-10-13 20:46:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,6 +43,13 @@ class Song
     # end
   end
 
+  def self.new_from_filename(filename)
+    song_name = filename.split(" - ")[1]
+    song = self.new(song_name)
+    song.artist = Artist.new(filename.split(" - ")[0])
+    song
+  end 
+
   def self.find_by_name(name)
     @@all.find{|song| song.name == name}
   end
