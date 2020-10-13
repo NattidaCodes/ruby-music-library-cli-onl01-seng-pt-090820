08447374acdc171048e5077b7337f3b43e3c161e
Nattida Wong <nattidac.wong@gmail.com>
2020-10-13 20:45:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,4 +51,11 @@ class Song
     self.find_by_name(name) || self.create(name)
   end
 
+  def self.new_by_filename(filename)
+    song_name = filename.split(" - ")[1]
+    song = self.new(song_name)
+    song.artist = Artist.new(filename.split(" - ")[0])
+    song
+  end 
+
 end
