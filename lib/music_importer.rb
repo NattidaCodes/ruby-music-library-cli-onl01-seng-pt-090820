557783ca468bc2704976a64022001f4ff89199e3
Nattida Wong <nattidac.wong@gmail.com>
2020-10-13 20:40:08 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,12 +1,16 @@
 class MusicImporter
 
   attr_accessor :path
+  attr_reader :files
 
   def initialize(path)
     @path = path
+    @files = []
+    @files << self
   end
 
   def files
+    @files
   end
 
 end
