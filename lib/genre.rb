6e556d15491296c5f46e73b1c64ef99747dc95d9
Nattida Wong <nattidac.wong@gmail.com>
2020-10-13 19:22:04 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,12 +1,12 @@
-class Genre
-
-  attr_accessor :name
-  attr_reader 
-
-  @@all = []
-
-  def self.all
-    @@all
-    end
-
-end
+# class Genre
+#
+#   attr_accessor :name
+#   attr_reader
+#
+#   @@all = []
+#
+#   def self.all
+#     @@all
+#     end
+#
+# end
