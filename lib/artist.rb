a3fc58eacc9c75382b4af0e96426a0fa65a0a22b
Nattida Wong <nattidac.wong@gmail.com>
2020-10-13 19:23:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,12 @@
-# class Artist
-#
-#   attr_accessor :name
-#   attr_reader :
-#
-#   @@all = []
-#
-#   def self.all
-#     @@all
-#     end
-#
-# end
+class Artist
+
+  attr_accessor :name
+  attr_reader :
+
+  @@all = []
+
+  def self.all
+    @@all
+    end
+
+end
